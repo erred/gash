0eda7e595fdd3701cc0c3bfe2aeed1d77eba0a2c
Sean Liao <seankhliao@gmail.com>
2019-02-18 17:40:10 +0800

    simpler

--- main.go
+++ main.go
@@ -1,124 +1,99 @@
 package main
 
 import (
+	"bytes"
 	"flag"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"net/http/httputil"
 	"net/url"
-	"path"
-	"strconv"
 	"strings"
 	"time"
 )
 
 var (
-	gtagURL = "https://www.googletagmanager.com/"
-	ptagURL = "https://gash.seankhliao.com/gtag/"
-	gaURL   = "https://www.google-analytics.com/"
-	paURL   = "https://gash.seankhliao.com/ga/"
+	gtagURL = "https://www.googletagmanager.com/gtag/js"
+	gaURL   = "https://www.google-analytics.com/analytics.js"
+	cURL    = "https://www.google-analytics.com/collect"
+	rsURL   = []byte("https://www.google-analytics.com/")
+	rtURL   = []byte("https://gash.seankhliao.com/")
 )
 
 func main() {
 	t := flag.Duration("t", 48*time.Hour, "update frequency of js, also used for cache age")
 	p := flag.String("p", "8080", "port to serve on")
 
-	s := NewScript(*t)
-	go s.tick()
+	gtag := NewScript(gtagURL)
+	go gtag.update(*t)
 
-	garp, err := NewReverseProxy(gaURL)
-	if err != nil {
-		log.Fatal("create ReverseProxy: ", err)
-	}
-	gtagrp, err := NewReverseProxy(gtagURL)
-	if err != nil {
-		log.Fatal("create ReverseProxy: ", err)
-	}
+	analytics := NewScript(gaURL)
+	go analytics.update(*t)
 
-	http.HandleFunc("/js", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/js" {
-			http.Redirect(w, r, "/js", http.StatusFound)
-			return
-		}
-
-		w.Header().Add("Cache-Control", "max-age="+strconv.Itoa(int(s.d.Seconds())))
-		w.Write(s.b)
-	})
-	http.Handle("/gtag", http.StripPrefix("/gtag", gtagrp))
-	http.Handle("/ga", http.StripPrefix("/ga", garp))
+	http.HandleFunc("/js", gtag)
+	http.HandleFunc("/analytics.js", analytics)
+	http.HandleFunc("/collect", collector)
 
 	http.ListenAndServe(":"+*p, nil)
 }
 
-func NewReverseProxy(target string) (*httputil.ReverseProxy, error) {
-	u, err := url.Parse(target)
-	if err != nil {
-		return nil, fmt.Errorf("parse url: %v", err)
-	}
-	director := func(req *http.Request) {
-		req.URL.Scheme = u.Scheme
-		req.URL.Host = u.Host
-		req.URL.Path = path.Join(u.Path, req.URL.Path)
-
-		vals := req.URL.Query()
-		tip := strings.Split(req.RemoteAddr, ":")[0]
-		if xff := req.Header.Get("X-Forwarded-For"); xff != "" {
-			tip = strings.Split(xff, ", ")[0]
-		}
-		vals.Add("uip", tip)
-		for k, v := range u.Query() {
-			for _, vv := range v {
-				vals.Add(k, vv)
-			}
-		}
-		req.URL.RawQuery = vals.Encode()
-
-		if _, ok := req.Header["User-Agent"]; !ok {
-			req.Header.Set("User-Agent", "")
-		}
-	}
-	return &httputil.ReverseProxy{Director: director}, nil
-}
-
 type Script struct {
-	d time.Duration // update
-	b []byte        // script bytes
+	u string
+	b []byte
 }
 
-func NewScript(d time.Duration) *Script {
+func NewScript(u string) *Script {
 	return &Script{
-		d: d,
+		u: u,
 	}
 }
 
-func (s *Script) tick() {
-	err := s.getGtag()
-	if err != nil {
-		log.Fatal("get script: ", err)
-	}
-	for range time.NewTicker(s.d).C {
-		err := s.getGtag()
-		if err != nil {
-			log.Println("get script: ", err)
-		}
+func (s *Script) update(d time.Duration) {
+	s.get()
+	for range time.NewTicker(d).C {
+		s.get()
 	}
 }
 
-func (s *Script) getGtag() error {
-	res, err := http.Get(gtagURL + "/gtag/js")
+func (s *Script) get() {
+	res, err := http.Get(s.u)
 	if err != nil {
-		return fmt.Errorf("http get: %v", err)
+		log.Println("get: ", err)
+	}
+	if res.StatusCode < 200 || res.StatusCode > 299 {
+		log.Println("get status: ", res.StatusCode, res.Status)
 	}
 	defer res.Body.Close()
 	b, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return fmt.Errorf("read response: %v", err)
+		log.Println("read get: ", err)
 	}
-	if res.StatusCode < 200 || res.StatusCode >= 300 {
-		return fmt.Errorf("status: %v", err)
+	s.b = bytes.Replace(b, rsURL, rtURL, -1)
+}
+
+func (s *Script) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write(s.b)
+}
+
+func collector(w http.ResponseWriter, r *http.Request) {
+	tip := strings.Split(r.RemoteAddr, ":")[0]
+	if xff := r.Header.Get("X-Forwarded-For"); xff != "" {
+		tip = strings.Split(xff, ", ")[0]
 	}
-	s.b = []byte(strings.NewReplacer(gaURL, paURL, gtagURL, ptagURL).Replace(string(b)))
-	return nil
+
+	vals := r.URL.Query()
+	vals.Add("uip", tip)
+	u, err := url.Parse(cURL)
+	if err != nil {
+		log.Println("parse url: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	u.RawQuery = vals.Encode()
+	res, err := http.Get(u.String())
+	if err != nil {
+		log.Println("get: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(res.StatusCode)
 }
